80f77041ee07ea3745d5ef26ea7d165bde7ebd30
DinkieShy <32346447+DinkieShy@users.noreply.github.com>
2023-10-10 22:17:53 +0100

    Generate pronunciations of names

--- generateNamePronunciations.py
+++ generateNamePronunciations.py
@@ -1,12 +1,16 @@
 from urllib.request import urlopen
 from string import ascii_lowercase
 import re
+from g2p_en import G2p
+from json import dump
+from tqdm import tqdm
 
 def getNames():
 	names = []
 	unfilteredNames = []
 
-	for char in ascii_lowercase:
+	print("Grabbing names...")
+	for char in tqdm(ascii_lowercase):
 		# Get the raw HTML
 		url = "https://www.1happybirthday.com/fyn/" + char
 		page = urlopen(url)
@@ -14,15 +18,15 @@ def getNames():
 
 		# Extract just the divs with the names
 		contentArea = html[html.find("<div id=\"content_block\">"):html.find("<div id=\"right_column\">")]
-		# Search through each line- lines beginning with "<a" have names, extract just the innerHTML part
-		unfilteredNames = unfilteredNames + [(item[item.find(">")+1:-10] if item[:2] == "<a" else None) for item in contentArea.split("\n")]
+		# Search through each line- only lines beginning with "<a" have names, extract just the innerHTML part
+		unfilteredNames = unfilteredNames + [(item[item.find(">")+1:-10]) for item in contentArea.split("\n") if item[:2] == "<a"]
 
-	# Lines that don't start with "<a" resulted in None being added to the array
 	# Some names have "NEW" or "2" at the end
 	# These all need to be filtered (NEW/2 are kept for finding the URL again later)
 	for name in unfilteredNames:
-		if name != None:
-			names.append([name, re.sub(r'[0-9]', "", name.replace("NEW", ""))])
+		names.append([name, re.sub(r'[0-9]', "", name.replace("NEW", ""))])
+
+	print(f"Grabbed {len(names)} names")
 
 	# This array contains [url appropriate name, pronouncable name]
 	return names
@@ -34,7 +38,19 @@ def main():
 
 	names = getNames()
 
-	print(len(names))
+	nameDict = {}
+
+	print("Generating pronunciations...")
+	g2p = G2p()
+	for item in tqdm(names):
+		pronunciation = [phone for phone in g2p(item[1])]
+		nameDict[item[0]] = {"filtered": item[1], "pronunciation": pronunciation}
+
+	print("Saving...")	
+	with open("pronunciations.json", "w") as file:
+		dump(nameDict, file)
+
+	print("Done!")
 
 if __name__ == "__main__":
 	main()
